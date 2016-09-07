bdf5bdb29b6f542cc5ee2315c333a682be6b7f1b
Cooper Knaak <cooperknaak@gmail.com>
2016-09-07 15:41:53 -0500

    Initial Commit

---
+++ CoronaGL/CoronaGL.h
@@ -0,0 +1,19 @@
+//
+//  CoronaGL.h
+//  CoronaGL
+//
+//  Created by Cooper Knaak on 9/7/16.
+//  Copyright © 2016 Cooper Knaak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoronaGL.
+FOUNDATION_EXPORT double CoronaGLVersionNumber;
+
+//! Project version string for CoronaGL.
+FOUNDATION_EXPORT const unsigned char CoronaGLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoronaGL/PublicHeader.h>
+
+
